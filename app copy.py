6fcff2466b6c5737ef080867d8f93076b9c8b10d
indraromdoni <indraromdoni65@gmail.com>
2024-11-15 11:01:12 +0700

    new modification

--- app copy.py
+++ app copy.py
@@ -21,7 +21,7 @@ dbconfig = {
 
 connection_pool = mysql.connector.pooling.MySQLConnectionPool(pool_name="mypool", pool_size=5, **dbconfig)
 
-chk_req_data = "SELECT * FROM trouble_equipment WHERE Status1=%s"
+chk_req_data = "SELECT * FROM trouble_equipment WHERE Status1 = %s"
 get_eqName = "SELECT ID_Equipment_Display, Equipment FROM equipment WHERE ID_Equipment = %s"
 
 cnt_req_before = 0
@@ -44,11 +44,17 @@ while True:
 
         if flag_newReq:
             txt = []
-            for data in res:
+            latest_trouble = res[-1]
+            cursor.execute(get_eqName, (latest_trouble[2],))
+            mc_name = cursor.fetchall()[0][1]
+            txt.append("SOLUMAN Information\n\nNew request occurs :\n")
+            txt.append(f"Req Num : {latest_trouble[0]}; Req From : {latest_trouble[20]}; M/C Name : {mc_name}; Trouble : {latest_trouble[4]}\n")
+            txt.append("\nWaiting for approval :\n")
+            for i, data in enumerate(res):
                 mc_id = data[2]
                 cursor.execute(get_eqName, (mc_id,))
                 mc_name = cursor.fetchall()[0][1]
-                txt.append(f"Req Num : {data[0]}; Req From : {data[20]}; M/C Name : {mc_name}; Trouble : {data[4]}\n")
+                txt.append(f"{i+1}. Req Num : {data[0]}; Req From : {data[20]}; M/C Name : {mc_name}; Trouble : {data[4]}\n")
             print("".join(txt))
             res = sendWA("6285794607446@c.us", "".join(txt))
             flag_newReq = 0
